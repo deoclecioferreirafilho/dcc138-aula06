d9a49c783f2708ec193eab07945db2d79ccec0f4
Deoclécio Filho <deoclecio@ice.ufjf.br>
2019-08-29 20:12:11 -0300

    Movimento do pc

--- Sprite.js
+++ Sprite.js
@@ -6,6 +6,9 @@ function Sprite(params = {}) {
         w: 10,
         vx: 0,
         vy: 0,
+        a: 0,
+        vm:0,
+        va: 0,
         color: "blue",
         imune: 0,
         atirando: 0,
@@ -18,14 +21,32 @@ Sprite.prototype = new Sprite({});
 Sprite.prototype.constructor = Sprite;
 
 Sprite.prototype.desenhar = function (ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.a);
     ctx.fillStyle = this.color;
     ctx.strokeStyle = "black";
     ctx.lineWidth = 1;
-    ctx.fillRect(this.x, this.y, this.w, this.h);
-    ctx.strokeRect(this.x, this.y, this.w, this.h);
+    //ctx.fillRect(-this.w/2, -this.h/2, this.w, this.h);
+    // ctx.strokeRect(-this.w/2, -this.h/2, this.w, this.h);
+
+    ctx.beginPath();
+    ctx.moveTo(-this.w / 2, -this.h / 2);
+    ctx.lineTo(-this.w / 2, +this.h / 2);
+    ctx.lineTo(+this.w / 2, 0)
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
 };
 
 Sprite.prototype.mover = function (dt) {
+    this.a = this.a + this.va * dt;
+
+    this.vx = this.vm* Math.cos(this.a);
+    this.vy = this.vm* Math.sin(this.a);
+
+
     this.x = this.x + this.vx * dt;
     this.y = this.y + this.vy * dt;
 }
